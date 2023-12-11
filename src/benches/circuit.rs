e0c8d5487daec9843e74888fc44e2f5a4f6a7eb0
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2023-12-11 14:36:29 -0500

    factored out compare function

--- src/benches/circuit.rs
+++ src/benches/circuit.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 mod tests {
     use std::env;
-    use log::{LevelFilter, debug};
+    use std::time::Instant;
+    use log::LevelFilter;
     use anyhow::Result;
     use plonky2::field::types::Field;
     use plonky2::iop::target::Target;
@@ -10,77 +11,128 @@ mod tests {
     use plonky2::plonk::circuit_data::CircuitConfig;
     use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 
-    // sets RUST_LOG=debug and initializes the logger
-    // call init() at the top of every comparison test
-    fn init() {
+    #[test]
+    fn compare_quin_random_access() -> Result<()> {
+        use crate::rlp::quin_selector;
+        use rand::Rng;
+
+        // common to both circuits
+        const D: usize = 2;
+        type C = PoseidonGoldilocksConfig;
+        type F = <C as GenericConfig<D>>::F;
+
+        // both versions of the circuit need to capture this information
+        let bits_of_length: usize = 6;
+        let byte_arr: Vec<u8> = (0..1<<bits_of_length).map(|_i| rand::thread_rng().gen()).collect();
+        println!("Array length: {}", byte_arr.len());
+        let rand_index: usize = rand::thread_rng().gen_range(0..1<<bits_of_length);
+
+        let config = CircuitConfig::standard_recursion_config();
+
+        let quin_version = |builder: &mut CircuitBuilder<F, D>| {
+            println!("QUIN VERSION");        
+            let arr_target: Vec<Target> = byte_arr
+                .iter()
+                .map(|x| builder.constant(F::from_canonical_u8(*x)))
+                .collect();
+            let n: Target = builder.constant(F::from_canonical_usize(rand_index));
+            let element = arr_target[rand_index];
+            
+            let ret_element = quin_selector( builder, &arr_target, n);
+            
+            builder.connect(element, ret_element);
+            builder.register_public_input(ret_element);
+            builder.register_public_inputs(&arr_target);
+        };
+
+        let random_access_version = |builder: &mut CircuitBuilder<F, D>| {
+            println!("RANDOM ACCESS VERSION");
+            let arr_target: Vec<Target> = byte_arr
+                .iter()
+                .map(|x| builder.constant(F::from_canonical_u8(*x)))
+                .collect();
+            let n: Target = builder.constant(F::from_canonical_usize(rand_index));
+            let element = arr_target[rand_index];
+
+            let ret_element = builder.random_access(n,arr_target.clone());
+
+            builder.connect(element, ret_element);
+            builder.register_public_input(ret_element);
+            builder.register_public_inputs(&arr_target);
+        };
+
+        // in this case there is nothing to do to the circuit
+        // after each version so we pass the identity function
+        compare::<C, D>(
+            config,
+            quin_version,
+            random_access_version,
+            |_| {}, // identity function
+        )
+    }    
+
+    /// Sets RUST_LOG=debug and initializes the logger
+    fn init_logging() {
         env::set_var("RUST_LOG", "debug");
         env_logger::init();
         log::set_max_level(LevelFilter::Debug);
     }
 
-    #[test]
-    fn compare_quin_random_access() -> Result<()> {
-        use crate::rlp::quin_selector;
-        use rand::Rng;
-        init();
-        const D: usize = 2;
-        type C = PoseidonGoldilocksConfig;
-        type F = <C as GenericConfig<D>>::F;
-        let config = CircuitConfig::standard_recursion_config();
-        let bits_of_length: usize = 6;
-        let byte_arr: Vec<u8> = (0..1<<bits_of_length).map(|_i| rand::thread_rng().gen()).collect();
-        println!("byte_arr length: {}", byte_arr.len());
-        let rand_index: usize = rand::thread_rng().gen_range(0..1<<bits_of_length);
+    /// Compares the gate counts, LDE size, build time, proving time, and verification time
+    /// of two circuits. Accepts two closures `v1` and `v2` which are the only places where
+    /// the two circuits are allowed to add different gates to the circuit. The `after` closure
+    /// can be used to add identical gates after the differences.
+    fn compare<C, const D: usize>(
+        config: CircuitConfig,
+        v1: impl Fn(&mut CircuitBuilder<C::F, D>),
+        v2: impl Fn(&mut CircuitBuilder<C::F, D>),
+        after: impl Fn(&mut CircuitBuilder<C::F, D>),
+    ) -> Result<()>
+    where
+        C: GenericConfig<D>,
+    {
+        // turn on logging and force DEBUG level logs
+        // to be printed to the screen
+        init_logging();
 
-        // quin version
-        debug!("QUIN VERSION");
-        let pw = PartialWitness::new();
-        let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+        // 
+        let end = |builder: CircuitBuilder<C::F, D>| {
+            // print gate information from the DEBUG log level
+            builder.print_gate_counts(0);
 
-        let arr_target: Vec<Target> = byte_arr
-            .iter()
-            .map(|x| builder.constant(F::from_canonical_u8(*x)))
-            .collect();
+            print!("Building....");
+            let now = Instant::now();
+            let data = builder.build::<C>();
+            println!("{:.2?}", now.elapsed());
 
-        let n: Target = builder.constant(F::from_canonical_usize(rand_index));
+            print!("Proving.....");         
+            let pw = PartialWitness::new();
+            let now = Instant::now();
+            let proof = data.prove(pw)?;
+            println!("{:.2?}", now.elapsed());
 
-        let element = arr_target[rand_index];
-        let ret_element = quin_selector(&mut builder, &arr_target, n);
+            print!("Verifying...");
+            let now = Instant::now();
+            let res = data.verify(proof);
+            println!("{:.2?}", now.elapsed());
 
-        builder.connect(element, ret_element);
-        builder.register_public_inputs(&arr_target);
-        builder.register_public_input(ret_element);
-        builder.print_gate_counts(0);
+            println!("LDE size: {}",data.common.lde_size());
 
-        let data = builder.build::<C>();
-        debug!("lde size: {}",data.common.lde_size());
-        let proof = data.prove(pw)?;
-        assert!(data.verify(proof).is_ok());
+            res
+        };
 
-        // random access version
-        debug!("RANDOM ACCESS VERSION");
-        let pw = PartialWitness::new();
-        let mut builder = CircuitBuilder::<F, D>::new(config);
+        let mut builder1 = CircuitBuilder::<C::F, D>::new(config.clone());
+        v1(&mut builder1);
+        after(&mut builder1);
+        let verified1 = end(builder1);
 
-        let arr_target: Vec<Target> = byte_arr
-            .iter()
-            .map(|x| builder.constant(F::from_canonical_u8(*x)))
-            .collect();
-
-        let n: Target = builder.constant(F::from_canonical_usize(rand_index));
-
-        let element = arr_target[rand_index];
-        let ret_element = builder.random_access(n,arr_target.clone());
-
-        builder.connect(element, ret_element);
-        builder.register_public_inputs(&arr_target);
-        builder.register_public_input(ret_element);
-        builder.print_gate_counts(0);
-
-        let data = builder.build::<C>();
-        debug!("lde size: {}",data.common.lde_size());
-        let proof = data.prove(pw)?;
-        data.verify(proof)
+        let mut builder2 = CircuitBuilder::<C::F, D>::new(config);
+        v2(&mut builder2);
+        after(&mut builder2);
+        let verified2 = end(builder2);
+        
+        assert!(verified1.is_ok());
+        assert!(verified2.is_ok());
+        verified1.and(verified2)
     }
-    
 }
