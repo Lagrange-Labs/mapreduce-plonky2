084b879b1a82280552da9f057e77ef5d62b34f8f
nikkolasg <nikkolasg@gmail.com>
2024-03-09 20:20:46 +0100

    fmt and adding file

--- mapreduce-plonky2/src/storage/mapping/mod.rs
+++ mapreduce-plonky2/src/storage/mapping/mod.rs
@@ -4,5 +4,5 @@ mod extension;
 mod leaf;
 mod public_inputs;
 
-pub use api::{CircuitInput, PublicParameters, build_circuits_params,generate_proof};
+pub use api::{build_circuits_params, generate_proof, CircuitInput, PublicParameters};
 pub use public_inputs::PublicInputs;
