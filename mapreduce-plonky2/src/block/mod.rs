f3f0048d3f687dc961771ee9b9412cff7d01f399
Steven <asongala@163.com>
2024-03-12 07:20:49 +0800

    Update mapreduce-plonky2/src/block/mod.rs
    Co-authored-by: Nicolas Gailly <nikkolasg@users.noreply.github.com>

--- mapreduce-plonky2/src/block/mod.rs
+++ mapreduce-plonky2/src/block/mod.rs
@@ -37,6 +37,7 @@ where
     /// The path starts from the sibling of new leaf (block), and the parent's
     /// siblings at each level, till to the root. It corresponds to the plonky2
     /// [MerkleProofTarget](https://github.com/0xPolygonZero/plonky2/blob/62ffe11a984dbc0e6fe92d812fa8da78b7ba73c7/plonky2/src/hash/merkle_proofs.rs#L37).
+    // The +1 is because it includes the root as well in the path. 
     path: [HashOutTarget; MAX_DEPTH + 1],
 }
 
