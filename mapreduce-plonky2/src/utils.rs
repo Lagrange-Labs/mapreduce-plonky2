f7ea39be8d31613e61d69a08fe05eec0c68b7052
Victor Lopez <victor@codx.io>
2024-03-12 18:07:35 +0100

    fix: block linking pi lengths
    This commit fixes inconsistencies on the defined lengths of the public
    inputs of the block linking circuit.
    
    Closes #110

--- mapreduce-plonky2/src/utils.rs
+++ mapreduce-plonky2/src/utils.rs
@@ -22,6 +22,8 @@ const TWO_POWER_24: usize = 16777216;
 pub const U64_LEN: usize = 8;
 /// Length of an U64 in U32
 pub const PACKED_U64_LEN: usize = U64_LEN / 4;
+/// Length aof a curve target (2x quintic + bool)
+pub const CURVE_TARGET_LEN: usize = 11;
 /// Byte representation of an U64
 pub type U64Target = Array<Target, U64_LEN>;
 /// U32 representation of an U64
