d8fb5c2eda2e0b87a8010c97612a6d5fe5a91022
Victor Lopes <victor@codx.io>
2024-03-12 19:18:00 +0300

    feat: add leaf node circuit (#105)
    * feat: add leaf node circuit
    
    This commit introduces the circuit for the leaf node. It will process a
    recursive proof of block header linkage and verify the authenticity of
    the leaf node's root preimage.
    
    Closes #66
    
    * extract lpn public inputs from #102
    
    * simplify lpn module structure and implementation
    
    * add circuit tests
    
    * fix wire structure to keccak hash output
    
    * remove duplicated wires from inner proof pis
    
    * fix panic comment
    
    * restore changes over block linking pi
    
    * simplify pi structure for leaf wires
    
    * rename target_from_seed
    
    * move block linking values to test leaf circuit
    
    * modularize leaf directory into lpn
    
    * remove leaf wire wrapper

---
+++ mapreduce-plonky2/src/state/lpn/leaf/tests.rs
@@ -0,0 +1,88 @@
+//! Tests module for the leaf circuit
+
+use plonky2::{
+    field::{goldilocks_field::GoldilocksField, types::Field},
+    hash::{
+        hash_types::HashOutTarget, hashing::hash_n_to_hash_no_pad, poseidon::PoseidonPermutation,
+    },
+    iop::{
+        target::Target,
+        witness::{PartialWitness, WitnessWrite},
+    },
+    plonk::{circuit_builder::CircuitBuilder, config::PoseidonGoldilocksConfig},
+};
+
+use crate::{
+    circuit::{test::run_circuit, UserCircuit},
+    state::{lpn::leaf::PublicInputs, BlockLinkingPublicInputs},
+};
+
+use super::LeafCircuit;
+
+#[test]
+fn prove_and_verify_leaf_circuit() {
+    let block_linking_values = BlockLinkingPublicInputs::values_from_seed(TestLeafCircuit::PI_SEED);
+    let block_linking = BlockLinkingPublicInputs::from_slice(&block_linking_values);
+
+    let preimage =
+        LeafCircuit::node_preimage(GoldilocksField::ONE, &block_linking).collect::<Vec<_>>();
+    let root = hash_n_to_hash_no_pad::<_, PoseidonPermutation<_>>(&preimage);
+
+    let circuit = TestLeafCircuit {
+        block_linking_values: block_linking_values.to_vec(),
+        c: LeafCircuit,
+    };
+    let proof = run_circuit::<_, _, PoseidonGoldilocksConfig, _>(circuit);
+    let pi = PublicInputs::from(proof.public_inputs.as_slice());
+
+    assert_eq!(pi.root_data(), root.elements);
+    assert_eq!(pi.block_header_data(), block_linking.block_hash());
+    assert_eq!(pi.block_number_data(), block_linking.block_number()[0]);
+    assert_eq!(pi.prev_block_header_data(), block_linking.prev_block_hash());
+}
+
+impl<'a, T: Clone> From<&'a [T]> for PublicInputs<'a, T> {
+    fn from(proof_inputs: &'a [T]) -> Self {
+        Self { proof_inputs }
+    }
+}
+
+#[derive(Clone)]
+struct TestLeafWires {
+    block_linking: Vec<Target>,
+    root: HashOutTarget,
+}
+
+#[derive(Clone)]
+struct TestLeafCircuit {
+    block_linking_values: Vec<GoldilocksField>,
+    c: LeafCircuit,
+}
+
+impl TestLeafCircuit {
+    const PI_SEED: u64 = 0xbeef;
+}
+
+impl UserCircuit<GoldilocksField, 2> for TestLeafCircuit {
+    type Wires = TestLeafWires;
+
+    fn build(b: &mut CircuitBuilder<GoldilocksField, 2>) -> Self::Wires {
+        let targets = b.add_virtual_targets(BlockLinkingPublicInputs::<()>::TOTAL_LEN);
+        let block_linking = BlockLinkingPublicInputs::from_slice(&targets);
+        let root = LeafCircuit::build(b, &block_linking);
+
+        TestLeafWires {
+            block_linking: targets.clone(),
+            root,
+        }
+    }
+
+    fn prove(&self, pw: &mut PartialWitness<GoldilocksField>, wires: &Self::Wires) {
+        let block_linking = BlockLinkingPublicInputs::from_slice(&wires.block_linking);
+
+        self.block_linking_values
+            .iter()
+            .zip(block_linking.inner.iter())
+            .for_each(|(&v, &t)| pw.set_target(t, v));
+    }
+}
