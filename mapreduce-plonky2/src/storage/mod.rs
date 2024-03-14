fbf41da4648d18aa3fcddfe4d5f700af3ff40498
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-03-14 20:15:19 +0100

    chore: prune old code

--- mapreduce-plonky2/src/storage/mod.rs
+++ mapreduce-plonky2/src/storage/mod.rs
@@ -16,27 +16,14 @@ pub(super) const MAX_LEAF_NODE_LEN: usize = MAX_EXTENSION_NODE_LEN;
 /// rlp( rlp(max key 32b) + rlp(max value 32b) ) + 1 for compact encoding
 /// see test_len()
 pub(crate) const MAX_EXTENSION_NODE_LEN: usize = 69;
-use plonky2::field::{goldilocks_field::GoldilocksField, types::Field};
 
 // Number of Goldilocks elements in CurveTarget
 const CURVE_TARGET_GL_SIZE: usize = 11;
 
 // A key is 32B-long
-const KEY_GL_SIZE: usize = 32;
+const KEY_SIZE: usize = 32;
 // A value in a leaf node is 32B wide
-const LEAF_GL_SIZE: usize = 32;
-// ['L', 'E', 'A', 'F'] -> 4B -> 1GL
-const LEAF_MARKER_GL_SIZE: usize = 1;
-
-#[allow(non_snake_case)]
-fn LEAF_MARKER() -> GoldilocksField {
-    GoldilocksField::from_canonical_u32(u32::from_be_bytes(*b"LEAF"))
-}
-
-#[allow(non_snake_case)]
-fn NODE_MARKER() -> GoldilocksField {
-    GoldilocksField::from_canonical_u32(u32::from_be_bytes(*b"NODE"))
-}
+const LEAF_SIZE: usize = 32;
 
 fn iter_curve_target<'a>(t: &'a CurveTarget) -> impl Iterator<Item = &'a Target> {
     t.0 .0
--- mapreduce-plonky2/src/storage/query2/leaf.rs
+++ mapreduce-plonky2/src/storage/query2/leaf.rs
@@ -11,20 +11,20 @@ use crate::{
     array::Array,
     circuit::UserCircuit,
     group_hashing::CircuitBuilderGroupHashing,
-    storage::{KEY_GL_SIZE, LEAF_GL_SIZE},
+    storage::{KEY_SIZE, LEAF_SIZE},
 };
 
 use super::{public_inputs::PublicInputs, AddressTarget};
 
 pub struct InclusionWires {
-    pub key: Array<Target, KEY_GL_SIZE>,
-    pub value: Array<Target, LEAF_GL_SIZE>,
+    pub key: Array<Target, KEY_SIZE>,
+    pub value: Array<Target, LEAF_SIZE>,
 }
 
 /// This circuit prove the new root hash of a leaf containing the requested data
 #[derive(Clone)]
 pub struct LeafCircuit {
-    pub key: [u8; KEY_GL_SIZE],
+    pub key: [u8; KEY_SIZE],
     pub value: [u8; AddressTarget::LEN],
 }
 
@@ -39,7 +39,7 @@ impl UserCircuit<GoldilocksField, 2> for LeafCircuit {
     type Wires = InclusionWires;
 
     fn build(b: &mut CircuitBuilder<GoldilocksField, 2>) -> Self::Wires {
-        let key = Array::<Target, KEY_GL_SIZE>::new(b);
+        let key = Array::<Target, KEY_SIZE>::new(b);
         let key_u32 = key.convert_u8_to_u32(b);
         let value = Array::<Target, { AddressTarget::LEN }>::new(b);
         let value_u32 = value.convert_u8_to_u32(b);
