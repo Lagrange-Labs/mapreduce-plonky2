c44e037d0988ecffa060c211c2bc620fa094abbd
nikkolasg <nikkolasg@gmail.com>
2024-03-14 23:58:19 +0100

    some comments

--- mapreduce-plonky2/src/storage/lpn/api.rs
+++ mapreduce-plonky2/src/storage/lpn/api.rs
@@ -20,16 +20,21 @@ use super::{
 const STORAGE_CIRCUIT_SET_SIZE: usize = 2;
 const NUM_IO: usize = PublicInputs::<F>::TOTAL_LEN;
 
+/// Inputs to the storage database related circuits (specifically for mapping)
 pub enum Inputs {
     Leaf(LeafCircuit),
     Node(NodeInputs),
 }
 
+/// The inputs for proving an intermediate node in the storage database
+/// for mapping.
 pub struct NodeInputs {
     left: Vec<u8>,
     right: Vec<u8>,
 }
 
+/// Parameters containing the public information for proving both leaves and nodes
+/// of the storage database
 #[derive(Serialize, Deserialize)]
 pub struct PublicParameters {
     leaf_circuit: CircuitWithUniversalVerifier<F, C, D, 0, LeafWires>,
@@ -38,6 +43,7 @@ pub struct PublicParameters {
 }
 
 impl PublicParameters {
+    /// Build the public parameters for the storage database related circuits
     pub fn build() -> Self {
         let config = CircuitConfig::standard_recursion_config();
         let circuit_builder = CircuitWithUniversalVerifierBuilder::<F, D, NUM_IO>::new::<C>(
@@ -47,7 +53,7 @@ impl PublicParameters {
         let leaf_circuit = circuit_builder.build_circuit::<C, 0, LeafWires>(());
         let node_circuit = circuit_builder.build_circuit::<C, 2, NodeWires>(());
 
-        let mut circuits_set = vec![
+        let circuits_set = vec![
             leaf_circuit.get_verifier_data().circuit_digest,
             node_circuit.get_verifier_data().circuit_digest,
         ];
@@ -60,6 +66,8 @@ impl PublicParameters {
         }
     }
 
+    /// Generate a proof for a leaf or node in the storage database and returns its
+    /// serialized form.
     pub fn generate_proof(&self, inputs: Inputs) -> Result<Vec<u8>> {
         match inputs {
             Inputs::Leaf(leaf) => {
@@ -96,18 +104,13 @@ mod test {
     use crate::storage::lpn::leaf::LeafCircuit;
     use crate::storage::lpn::KEY_SIZE;
     use crate::storage::lpn::LEAF_SIZE;
-    use plonky2::plonk::circuit_builder::CircuitBuilder;
-    use plonky2::plonk::circuit_data::CircuitConfig;
-    use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
-    use plonky2::plonk::proof::ProofWithPublicInputs;
-    use recursion_framework::circuit_builder::CircuitWithUniversalVerifierBuilder;
 
     #[test]
     fn test_public_parameters() {
         let gen_input = |k: &'_ str, v: &'_ str| -> ([u8; KEY_SIZE], [u8; LEAF_SIZE]) {
             let kb = left_pad32(k.as_bytes());
             let vb = left_pad32(v.as_bytes());
-            (kb.try_into().unwrap(), vb.try_into().unwrap())
+            (kb, vb)
         };
         let (k1, v1) = gen_input("deadbeef", "0badf00d");
         let (k2, v2) = gen_input("0badf00d", "deedbaaf");
