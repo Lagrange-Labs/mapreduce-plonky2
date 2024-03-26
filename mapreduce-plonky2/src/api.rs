303724b436f3b7556b469145db4135c7ad4ea02a
nicholas-mainardi <mainardinicholas@gmail.com>
2024-03-26 19:15:14 +0100

    Fix recursive verifier to hardcode verifier data

--- mapreduce-plonky2/src/api.rs
+++ mapreduce-plonky2/src/api.rs
@@ -125,23 +125,15 @@ pub fn generate_proof<const MAX_DEPTH: usize>(
         }
         CircuitInput::LengthMatch(length_match_input) => {
             let (mapping_proof, length_proof) = length_match_input.try_into()?;
-            let length_match_proof = ProofWithVK::from((
-                length_proof,
-                params.length_extract.circuit_data().verifier_only.clone(),
-            ));
             params.length_match.generate_proof(
                 params.mapping.get_mapping_circuit_set(),
                 &mapping_proof,
-                &length_match_proof,
+                &length_proof,
             )
         }
         CircuitInput::Storage(storage_input) => params.lpn_storage.generate_proof(storage_input),
         CircuitInput::DigestEqual(digest_equal_input) => {
             let (lpn_proof, mpt_proof) = digest_equal_input.try_into()?;
-            let mpt_proof = ProofWithVK::from((
-                mpt_proof,
-                params.length_match.circuit_data().verifier_only.clone(),
-            ));
             params.digest_equal.generate_proof(
                 params.lpn_storage.get_lpn_circuit_set(),
                 &lpn_proof,
@@ -149,21 +141,13 @@ pub fn generate_proof<const MAX_DEPTH: usize>(
             )
         }
         CircuitInput::BlockLinking(block_linking_input) => {
-            let storage_proof = ProofWithVK::from((
-                block_linking_input.storage_proof.clone(),
-                params.digest_equal.circuit_data().verifier_only.clone(),
-            ));
-            params.block_linking.generate_proof(
-                &block_linking_input,
-                &params.digest_equal.circuit_data().verifier_only,
-            )
+            params.block_linking.generate_proof(&block_linking_input)
         }
         CircuitInput::State(state_input) => {
             let proof_input = match state_input {
-                lpn_state::api::CircuitInput::Leaf(leaf_proof) => ProofInputs::from_leaf_input(
-                    leaf_proof,
-                    &params.block_linking.circuit_data().verifier_only,
-                ),
+                lpn_state::api::CircuitInput::Leaf(leaf_proof) => {
+                    ProofInputs::from_leaf_input(leaf_proof)
+                }
                 lpn_state::api::CircuitInput::Node((left, right)) => {
                     ProofInputs::from_node_input(&left, &right)
                 }
@@ -281,8 +265,32 @@ impl<'a>
     }
 }
 
+/// Recursively verify a proof for a circuit with the given `verifier_data`
+pub(crate) fn verify_proof_fixed_circuit<
+    F: SerializableRichField<D>,
+    C: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    cb: &mut CircuitBuilder<F, D>,
+    verifier_data: &VerifierCircuitData<F, C, D>,
+) -> ProofWithPublicInputsTarget<D>
+where
+    C::Hasher: AlgebraicHasher<F>,
+{
+    let proof = cb.add_virtual_proof_with_pis(&verifier_data.common);
+    let vd = VerifierCircuitTarget {
+        constants_sigmas_cap: cb
+            .constant_merkle_cap(&verifier_data.verifier_only.constants_sigmas_cap),
+        circuit_digest: cb.constant_hash(verifier_data.verifier_only.circuit_digest),
+    };
+    cb.verify_proof::<C>(&proof, &vd, &verifier_data.common);
+    proof
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
+    use plonky2::{field::types::Field, iop::target::Target};
+
     use super::*;
     use anyhow::Result;
     use plonky2::{
@@ -329,5 +337,109 @@ pub(crate) mod tests {
         pub(crate) fn circuit_data(&self) -> &CircuitData<F, C, D> {
             &self.data
         }
+    }
+
+    // check that the closure $f actually panics, printing $msg as error message if the function
+    // did not panic; this macro is employed in tests in place of #[should_panic] to ensure that a
+    // panic occurred in the expected function rather than in other parts of the test
+    macro_rules! check_panic {
+        ($f: expr, $msg: expr) => {{
+            let result = std::panic::catch_unwind(std::panic::AssertUnwindSafe($f));
+            assert!(result.is_err(), $msg);
+        }};
+    }
+
+    #[test]
+    fn test_verify_proof_with_fixed_circuit() {
+        const NUM_IO: usize = 4;
+        struct TestCircuit {
+            data: CircuitData<F, C, D>,
+            pi_targets: [Target; NUM_IO],
+        }
+
+        impl TestCircuit {
+            fn build(config: CircuitConfig, value: usize) -> Self {
+                let mut builder = CircuitBuilder::<F, D>::new(config);
+                let val_t = builder.constant(F::from_canonical_usize(value));
+                let pi_targets = builder.add_virtual_public_input_arr::<NUM_IO>();
+                builder.connect(pi_targets[0], val_t);
+
+                let data = builder.build::<C>();
+                TestCircuit { data, pi_targets }
+            }
+
+            fn prove(&self, value: usize) -> Result<ProofWithPublicInputs<F, C, D>> {
+                let mut pw = PartialWitness::<F>::new();
+                let values = [F::from_canonical_usize(value); NUM_IO];
+                pw.set_target_arr(&self.pi_targets, &values);
+                self.data.prove(pw)
+            }
+        }
+
+        let config = CircuitConfig::standard_recursion_config();
+
+        let first_test_circuit = TestCircuit::build(config.clone(), 42);
+        let second_test_circuit = TestCircuit::build(config.clone(), 24);
+        // check that the 2 circuits has the same `CommonCircuitData`, but different `VerifierOnlyCircuitData`
+        assert_eq!(
+            first_test_circuit.data.common,
+            second_test_circuit.data.common
+        );
+        assert_ne!(
+            first_test_circuit.data.verifier_only,
+            second_test_circuit.data.verifier_only
+        );
+
+        let first_base_proof = first_test_circuit.prove(42).unwrap();
+        let second_base_proof = second_test_circuit.prove(24).unwrap();
+
+        struct VerifierCircuit {
+            data: CircuitData<F, C, D>,
+            proof: ProofWithPublicInputsTarget<D>,
+        }
+
+        impl VerifierCircuit {
+            fn build(config: CircuitConfig, vd: &VerifierCircuitData<F, C, D>) -> Self {
+                let mut builder = CircuitBuilder::<F, D>::new(config);
+                let proof = verify_proof_fixed_circuit(&mut builder, vd);
+                let data = builder.build::<C>();
+
+                Self { data, proof }
+            }
+
+            fn prove(
+                &self,
+                proof: &ProofWithPublicInputs<F, C, D>,
+            ) -> Result<ProofWithPublicInputs<F, C, D>> {
+                let mut pw = PartialWitness::<F>::new();
+                pw.set_proof_with_pis_target(&self.proof, proof);
+                self.data.prove(pw)
+            }
+        }
+
+        // build verifier circuit for `test_circuit`
+        let first_verifier =
+            VerifierCircuit::build(config.clone(), &first_test_circuit.data.verifier_data());
+        // recursive verification of proof of `test_circuit` should work
+        let rec_proof = first_verifier.prove(&first_base_proof).unwrap();
+        first_verifier.data.verify(rec_proof).unwrap();
+
+        // verify proof of `second_test_circuit` shouldn't work
+        check_panic!(
+            || first_verifier.prove(&second_base_proof),
+            "successful recursive verification of proof for second circuit with first verifier"
+        );
+
+        // check that `second_base_proof` can be verifier by verifier for `second_test_circuit`
+        let second_verifier =
+            VerifierCircuit::build(config.clone(), &second_test_circuit.data.verifier_data());
+        let rec_proof = second_verifier.prove(&second_base_proof).unwrap();
+        second_verifier.data.verify(rec_proof).unwrap();
+
+        // verify proof of `test_circuit` shouldn't work
+        check_panic!(
+            || second_verifier.prove(&first_base_proof),
+            "successful recursive verification of proof for second circuit with first verifier"
+        );
     }
 }
