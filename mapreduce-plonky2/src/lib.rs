395369884e3e477b968558e956a7ad1b8761fc8b
Steven Gu <asongala@163.com>
2024-05-01 11:14:44 +0800

    Add a comment to describe the re-importing is only used for `v0`.

--- mapreduce-plonky2/src/lib.rs
+++ mapreduce-plonky2/src/lib.rs
@@ -1,12 +1,13 @@
 //! Plonky2 documentation
 
-// TODO: enable it later.
+// TODO: should update and enable it.
 // #![warn(missing_docs)]
 #![feature(generic_const_exprs)]
 #![feature(generic_arg_infer)]
 #![feature(const_for)]
 #![feature(generic_const_items)]
 
+// NOTE: it's convenient to only avoid changing dependencies in each source file for v0.
 use common::{array, group_hashing, keccak, merkle_tree, mpt_sequential, poseidon, rlp};
 pub use common::{eth, types, utils};
 
