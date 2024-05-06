8c42bf4a8e02b138468ee9fafb1ea6bca564e064
Steven Gu <asongala@163.com>
2024-05-06 18:38:08 +0800

    Add value extractions circuits.

---
+++ mp2-v1/src/values_extraction/extension.rs
@@ -0,0 +1,237 @@
+//! Module handling the extension node inside a storage trie
+
+use super::{public_inputs::PublicInputs, MAX_EXTENSION_NODE_LEN};
+use anyhow::Result;
+use mp2_common::{
+    array::{Vector, VectorWire},
+    keccak::{InputData, KeccakCircuit, KeccakWires, HASH_LEN},
+    mpt_sequential::{MPTLeafOrExtensionNode, PAD_LEN},
+    types::{CBuilder, GFp},
+};
+use plonky2::{
+    iop::{target::Target, witness::PartialWitness},
+    plonk::proof::ProofWithPublicInputsTarget,
+};
+use recursion_framework::circuit_builder::CircuitLogicWires;
+use serde::{Deserialize, Serialize};
+
+const PADDED_LEN: usize = PAD_LEN(MAX_EXTENSION_NODE_LEN);
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct ExtensionNodeWires {
+    node: VectorWire<Target, PADDED_LEN>,
+    root: KeccakWires<PADDED_LEN>,
+}
+
+/// Circuit to proving the processing of an extension node
+#[derive(Clone, Debug)]
+pub struct ExtensionNodeCircuit {
+    pub(crate) node: Vec<u8>,
+}
+
+impl ExtensionNodeCircuit {
+    pub fn build(b: &mut CBuilder, child_proof: PublicInputs<Target>) -> ExtensionNodeWires {
+        let ttrue = b._true();
+
+        // Build the node wires.
+        let wires = MPTLeafOrExtensionNode::build_and_advance_key::<
+            _,
+            2,
+            MAX_EXTENSION_NODE_LEN,
+            HASH_LEN,
+        >(b, &child_proof.mpt_key());
+        let node = wires.node;
+        let root = wires.root;
+
+        // Constrain the extracted hash is the one exposed by the proof.
+        let packed_child_hash = wires.value.convert_u8_to_u32(b);
+        let given_child_hash = child_proof.root_hash();
+        let equals = packed_child_hash.equals(b, &given_child_hash);
+        b.connect(ttrue.target, equals.target);
+
+        // Expose the public inputs.
+        PublicInputs::register(
+            b,
+            &root.output_array,
+            &wires.key,
+            child_proof.values_digest(),
+            child_proof.metadata_digest(),
+            child_proof.n(),
+        );
+
+        ExtensionNodeWires { node, root }
+    }
+
+    pub fn assign(&self, pw: &mut PartialWitness<GFp>, wires: &ExtensionNodeWires) {
+        let node = Vector::<u8, PADDED_LEN>::from_vec(&self.node).unwrap();
+        wires.node.assign(pw, &node);
+
+        KeccakCircuit::<PADDED_LEN>::assign(pw, &wires.root, &InputData::Assigned(&node));
+    }
+}
+
+/// D = 2,
+/// Num of children = 1
+impl CircuitLogicWires<GFp, 2, 1> for ExtensionNodeWires {
+    type CircuitBuilderParams = ();
+
+    type Inputs = ExtensionNodeCircuit;
+
+    const NUM_PUBLIC_INPUTS: usize = PublicInputs::<GFp>::TOTAL_LEN;
+
+    fn circuit_logic(
+        builder: &mut CBuilder,
+        verified_proofs: [&ProofWithPublicInputsTarget<2>; 1],
+        _builder_parameters: Self::CircuitBuilderParams,
+    ) -> Self {
+        let inputs = PublicInputs::new(&verified_proofs[0].public_inputs);
+        ExtensionNodeCircuit::build(builder, inputs)
+    }
+
+    fn assign_input(&self, inputs: Self::Inputs, pw: &mut PartialWitness<GFp>) -> Result<()> {
+        inputs.assign(pw, self);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{super::public_inputs::tests::new_extraction_public_inputs, *};
+    use eth_trie::{EthTrie, MemoryDB, Nibbles, Trie};
+    use mp2_common::{
+        group_hashing::map_to_curve_point,
+        rlp::MAX_KEY_NIBBLE_LEN,
+        utils::{convert_u8_to_u32_slice, keccak256},
+        C, D, F,
+    };
+    use mp2_test::{
+        circuit::{run_circuit, UserCircuit},
+        utils::random_vector,
+    };
+    use plonky2::{
+        field::{goldilocks_field::GoldilocksField, types::Field},
+        iop::{target::Target, witness::WitnessWrite},
+        plonk::{
+            circuit_builder::CircuitBuilder,
+            config::{GenericConfig, PoseidonGoldilocksConfig},
+        },
+    };
+    use rand::{thread_rng, Rng};
+    use std::{panic, sync::Arc};
+
+    #[derive(Clone, Debug)]
+    struct TestExtensionNodeCircuit<'a> {
+        c: ExtensionNodeCircuit,
+        exp_pi: PublicInputs<'a, F>,
+    }
+
+    impl<'a> UserCircuit<F, D> for TestExtensionNodeCircuit<'a> {
+        // Extension node wires + child public inputs
+        type Wires = (ExtensionNodeWires, Vec<Target>);
+
+        fn build(b: &mut CircuitBuilder<F, D>) -> Self::Wires {
+            let exp_pi = b.add_virtual_targets(PublicInputs::<Target>::TOTAL_LEN);
+            let ext_wires = ExtensionNodeCircuit::build(b, PublicInputs::new(&exp_pi));
+
+            (ext_wires, exp_pi)
+        }
+
+        fn prove(&self, pw: &mut PartialWitness<F>, wires: &Self::Wires) {
+            self.c.assign(pw, &wires.0);
+
+            assert_eq!(wires.1.len(), PublicInputs::<Target>::TOTAL_LEN);
+            assert_eq!(
+                self.exp_pi.proof_inputs.len(),
+                PublicInputs::<Target>::TOTAL_LEN
+            );
+            pw.set_target_arr(&wires.1, self.exp_pi.proof_inputs)
+        }
+    }
+
+    #[test]
+    fn test_values_extraction_extension_node_circuit() {
+        // We need to create a trie that for sure contains an extension node:
+        // We insert two values under two keys which only differ by their last nibble/byte
+        // Normally, the trie should look like:
+        // root = extension node
+        // branch = point of different between the two keys
+        // two leaves
+        let memdb = Arc::new(MemoryDB::new(true));
+        let mut trie = EthTrie::new(Arc::clone(&memdb));
+        let key1 = random_vector(32);
+        let mut key2 = key1.clone();
+        key2[31] = thread_rng().gen();
+        let value1 = random_vector(32);
+        let value2 = random_vector(32);
+        trie.insert(&key1, &value1).unwrap();
+        trie.insert(&key2, &value2).unwrap();
+        trie.root_hash().unwrap();
+        let proof = trie.get_proof(&key1).unwrap();
+        let node = proof.first().unwrap().clone();
+        let root_rlp: Vec<Vec<u8>> = rlp::decode_list(&node);
+        assert_eq!(root_rlp.len(), 2);
+
+        // Prepare the public inputs for the extension node circuit.
+        let [values_digest, metadata_digest] = [random_vector(10), random_vector(20)].map(|arr| {
+            map_to_curve_point(
+                &arr.into_iter()
+                    .map(F::from_canonical_u8)
+                    .collect::<Vec<_>>(),
+            )
+            .to_weierstrass()
+        });
+        let key = random_vector(64);
+        let ptr = 63;
+        // Hash the child of the extension node in packed mode.
+        let child_hash = convert_u8_to_u32_slice(&keccak256(&proof[1]));
+        let n = 15;
+        let exp_pi = new_extraction_public_inputs(
+            &child_hash,
+            &key,
+            ptr,
+            &values_digest,
+            &metadata_digest,
+            n,
+        );
+        let exp_pi = PublicInputs::new(&exp_pi);
+
+        // Quick test to see if we can convert back to public inputs.
+        assert_eq!(child_hash, exp_pi.root_hash());
+        let (exp_key, exp_ptr) = exp_pi.mpt_key_info();
+        assert_eq!(
+            key.iter()
+                .cloned()
+                .map(F::from_canonical_u8)
+                .collect::<Vec<_>>(),
+            exp_key,
+        );
+        assert_eq!(values_digest, exp_pi.values_digest());
+        assert_eq!(metadata_digest, exp_pi.metadata_digest());
+        assert_eq!(F::from_canonical_usize(n), exp_pi.n());
+
+        let circuit = TestExtensionNodeCircuit {
+            c: ExtensionNodeCircuit { node: node.clone() },
+            exp_pi: exp_pi.clone(),
+        };
+        let proof = run_circuit::<F, D, C, _>(circuit);
+        let pi = PublicInputs::new(&proof.public_inputs);
+
+        {
+            let exp_hash = keccak256(&node);
+            let exp_hash = convert_u8_to_u32_slice(&exp_hash);
+            assert_eq!(pi.root_hash(), exp_hash);
+        }
+        {
+            let (key, ptr) = pi.mpt_key_info();
+            assert_eq!(key, exp_key);
+
+            let ext_key: Vec<Vec<u8>> = rlp::decode_list(&node);
+            let nib = Nibbles::from_compact(&ext_key[0]);
+            let exp_ptr = F::from_canonical_usize(MAX_KEY_NIBBLE_LEN - 1 - nib.nibbles().len());
+            assert_eq!(ptr, exp_ptr);
+        }
+        assert_eq!(pi.values_digest(), exp_pi.values_digest());
+        assert_eq!(pi.metadata_digest(), exp_pi.metadata_digest());
+        assert_eq!(pi.n(), exp_pi.n());
+    }
+}
---
+++ mp2-v1/src/values_extraction/leaf_mapping.rs
@@ -0,0 +1,319 @@
+//! Module handling the mapping entries inside a storage trie
+
+use super::{
+    key::{MappingSlot, MappingSlotWires},
+    public_inputs::PublicInputs,
+    KEY_ID_PREFIX, MAX_LEAF_NODE_LEN, VALUE_ID_PREFIX,
+};
+use mp2_common::{
+    array::{Array, Vector, VectorWire},
+    group_hashing::CircuitBuilderGroupHashing,
+    keccak::{InputData, KeccakCircuit, KeccakWires},
+    mpt_sequential::{
+        utils::left_pad_leaf_value, MPTLeafOrExtensionNode, MAX_LEAF_VALUE_LEN, PAD_LEN,
+    },
+    types::{CBuilder, GFp, MAPPING_KEY_LEN, MAPPING_LEAF_VALUE_LEN},
+    utils::{convert_u8_targets_to_u32, pack_and_compute_poseidon_target},
+};
+use plonky2::{
+    field::types::Field,
+    iop::{target::Target, witness::PartialWitness},
+    plonk::circuit_builder::CircuitBuilder,
+};
+use plonky2_ecgfp5::gadgets::curve::CircuitBuilderEcGFp5;
+use recursion_framework::circuit_builder::CircuitLogicWires;
+use serde::{Deserialize, Serialize};
+use std::iter;
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub(crate) struct LeafMappingWires<const NODE_LEN: usize>
+where
+    [(); PAD_LEN(NODE_LEN)]:,
+{
+    node: VectorWire<Target, { PAD_LEN(NODE_LEN) }>,
+    root: KeccakWires<{ PAD_LEN(NODE_LEN) }>,
+    slot: MappingSlotWires,
+    value: Array<Target, MAPPING_LEAF_VALUE_LEN>,
+}
+
+impl<const N: usize> LeafMappingWires<N>
+where
+    [(); PAD_LEN(N)]:,
+{
+    pub fn mapping_key(&self) -> Array<Target, MAPPING_KEY_LEN> {
+        self.slot.mapping_key.clone()
+    }
+
+    pub fn mapping_slot(&self) -> Target {
+        self.slot.mapping_slot
+    }
+}
+
+/// Circuit to prove the correct derivation of the MPT key from a mapping slot
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct LeafMappingCircuit<const NODE_LEN: usize> {
+    pub(crate) node: Vec<u8>,
+    pub(crate) slot: MappingSlot,
+}
+
+impl<const NODE_LEN: usize> LeafMappingCircuit<NODE_LEN>
+where
+    [(); PAD_LEN(NODE_LEN)]:,
+{
+    pub fn build(b: &mut CBuilder) -> LeafMappingWires<NODE_LEN> {
+        let slot = MappingSlot::mpt_key(b);
+
+        // Build the node wires.
+        let wires =
+            MPTLeafOrExtensionNode::build_and_advance_key::<_, 2, NODE_LEN, MAX_LEAF_VALUE_LEN>(
+                b,
+                &slot.keccak_mpt.mpt_key,
+            );
+        let node = wires.node;
+        let root = wires.root;
+
+        // Left pad the leaf value.
+        let value = left_pad_leaf_value(b, &wires.value);
+
+        // Compute the key and value IDs - Poseidon(KEY || slot) and Poseidon(VAL || slot).
+        let [key_id, value_id] = [KEY_ID_PREFIX, VALUE_ID_PREFIX].map(|prefix| {
+            let prefix: Vec<_> = prefix.iter().cloned().map(GFp::from_canonical_u8).collect();
+            let prefix = b.constants(&prefix);
+
+            let inputs: Vec<_> = prefix
+                .into_iter()
+                .chain(iter::once(slot.mapping_slot))
+                .collect();
+            assert_eq!(inputs.len(), 4);
+
+            pack_and_compute_poseidon_target(b, &inputs).elements
+        });
+
+        // Compute the metadata digest - D(key_id || value_id || slot).
+        let inputs: Vec<_> = key_id
+            .iter()
+            .cloned()
+            .chain(value_id)
+            .chain([slot.mapping_slot])
+            .collect();
+        assert_eq!(inputs.len(), 9);
+
+        let metadata_digest = b.map_to_curve_point(&inputs);
+
+        // Compute the values digest - D(D(key_id || key) + D(value_id || value)).
+        assert_eq!(slot.mapping_key.arr.len(), MAPPING_KEY_LEN);
+        assert_eq!(value.arr.len(), MAPPING_LEAF_VALUE_LEN);
+        let [packed_key, packed_value] = [&slot.mapping_key, &value].map(|arr| {
+            convert_u8_targets_to_u32(b, &arr.arr)
+                .into_iter()
+                .map(|t| t.0)
+                .collect::<Vec<_>>()
+        });
+        let inputs: Vec<_> = key_id.into_iter().chain(packed_key).collect();
+        let k_digest = b.map_to_curve_point(&inputs);
+        let inputs: Vec<_> = value_id.into_iter().chain(packed_value).collect();
+        let v_digest = b.map_to_curve_point(&inputs);
+        // D(key_id || key) + D(value_id || value)
+        let add_digest = b.curve_add(k_digest, v_digest);
+        let inputs: Vec<_> = add_digest
+            .0
+             .0
+            .into_iter()
+            .flat_map(|ext| ext.0)
+            .chain(iter::once(k_digest.0 .1.target))
+            .collect();
+        let values_digest = b.map_to_curve_point(&inputs);
+
+        // Only one leaf in this node.
+        let n = b.one();
+
+        // Register the public inputs.
+        PublicInputs::register(
+            b,
+            &root.output_array,
+            &wires.key,
+            values_digest,
+            metadata_digest,
+            n,
+        );
+
+        LeafMappingWires {
+            node,
+            root,
+            slot,
+            value,
+        }
+    }
+
+    pub fn assign(&self, pw: &mut PartialWitness<GFp>, wires: &LeafMappingWires<NODE_LEN>) {
+        let pad_node =
+            Vector::<u8, { PAD_LEN(NODE_LEN) }>::from_vec(&self.node).expect("invalid node given");
+        wires.node.assign(pw, &pad_node);
+        KeccakCircuit::<{ PAD_LEN(NODE_LEN) }>::assign(
+            pw,
+            &wires.root,
+            &InputData::Assigned(&pad_node),
+        );
+        self.slot.assign(pw, &wires.slot);
+    }
+}
+
+/// D = 2,
+/// Num of children = 0
+impl CircuitLogicWires<GFp, 2, 0> for LeafMappingWires<MAX_LEAF_NODE_LEN> {
+    type CircuitBuilderParams = ();
+
+    type Inputs = LeafMappingCircuit<MAX_LEAF_NODE_LEN>;
+
+    const NUM_PUBLIC_INPUTS: usize = PublicInputs::<GFp>::TOTAL_LEN;
+
+    fn circuit_logic(
+        builder: &mut CircuitBuilder<GFp, 2>,
+        _verified_proofs: [&plonky2::plonk::proof::ProofWithPublicInputsTarget<2>; 0],
+        _builder_parameters: Self::CircuitBuilderParams,
+    ) -> Self {
+        LeafMappingCircuit::build(builder)
+    }
+
+    fn assign_input(
+        &self,
+        inputs: Self::Inputs,
+        pw: &mut PartialWitness<GFp>,
+    ) -> anyhow::Result<()> {
+        inputs.assign(pw, self);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        super::{
+            compute_leaf_mapping_key_id, compute_leaf_mapping_metadata_digest,
+            compute_leaf_mapping_value_id, compute_leaf_mapping_values_digest,
+        },
+        *,
+    };
+
+    use eth_trie::{Nibbles, Trie};
+    use mp2_common::{
+        array::Array,
+        eth::{left_pad32, StorageSlot},
+        group_hashing::map_to_curve_point,
+        mpt_sequential::utils::bytes_to_nibbles,
+        rlp::MAX_KEY_NIBBLE_LEN,
+        utils::{convert_u8_to_u32_slice, keccak256, pack_and_compute_poseidon_value},
+        C, D, F,
+    };
+    use mp2_test::{
+        circuit::{run_circuit, UserCircuit},
+        mpt_sequential::generate_random_storage_mpt,
+        utils::random_vector,
+    };
+    use plonky2::{
+        field::types::Field,
+        iop::{target::Target, witness::PartialWitness},
+        plonk::{
+            circuit_builder::CircuitBuilder,
+            config::{GenericConfig, PoseidonGoldilocksConfig},
+        },
+    };
+
+    #[derive(Clone, Debug)]
+    struct TestLeafMappingCircuit<const NODE_LEN: usize> {
+        c: LeafMappingCircuit<NODE_LEN>,
+        exp_value: Vec<u8>,
+    }
+
+    impl<const NODE_LEN: usize> UserCircuit<F, D> for TestLeafMappingCircuit<NODE_LEN>
+    where
+        [(); PAD_LEN(NODE_LEN)]:,
+    {
+        // Leaf wires + expected extracted value
+        type Wires = (
+            LeafMappingWires<NODE_LEN>,
+            Array<Target, MAPPING_LEAF_VALUE_LEN>,
+        );
+
+        fn build(b: &mut CircuitBuilder<F, D>) -> Self::Wires {
+            let exp_value = Array::<Target, MAPPING_LEAF_VALUE_LEN>::new(b);
+
+            let leaf_wires = LeafMappingCircuit::<NODE_LEN>::build(b);
+            leaf_wires.value.enforce_equal(b, &exp_value);
+
+            (leaf_wires, exp_value)
+        }
+
+        fn prove(&self, pw: &mut PartialWitness<F>, wires: &Self::Wires) {
+            self.c.assign(pw, &wires.0);
+            wires
+                .1
+                .assign_bytes(pw, &self.exp_value.clone().try_into().unwrap());
+        }
+    }
+
+    #[test]
+    fn test_values_extraction_leaf_mapping_circuit() {
+        const NODE_LEN: usize = 80;
+
+        let mapping_slot = 2_u8;
+        let mapping_key = hex::decode("1234").unwrap();
+        let slot = StorageSlot::Mapping(mapping_key.clone(), mapping_slot as usize);
+
+        let (mut trie, _) = generate_random_storage_mpt::<3, MAPPING_LEAF_VALUE_LEN>();
+        let value = random_vector(MAPPING_LEAF_VALUE_LEN);
+        let encoded_value: Vec<u8> = rlp::encode(&value).to_vec();
+        trie.insert(&slot.mpt_key(), &encoded_value).unwrap();
+        trie.root_hash().unwrap();
+
+        let proof = trie.get_proof(&slot.mpt_key_vec()).unwrap();
+        let node = proof.last().unwrap().clone();
+
+        let c = LeafMappingCircuit::<NODE_LEN> {
+            node: node.clone(),
+            slot: MappingSlot::new(mapping_slot, mapping_key.clone()),
+        };
+        let test_circuit = TestLeafMappingCircuit {
+            c,
+            exp_value: value.clone(),
+        };
+
+        let proof = run_circuit::<F, D, C, _>(test_circuit);
+        let pi = PublicInputs::new(&proof.public_inputs);
+
+        {
+            let exp_hash = keccak256(&node);
+            let exp_hash = convert_u8_to_u32_slice(&exp_hash);
+            assert_eq!(pi.root_hash(), exp_hash);
+        }
+        {
+            let (key, ptr) = pi.mpt_key_info();
+
+            let exp_key = slot.mpt_key_vec();
+            let exp_key: Vec<_> = bytes_to_nibbles(&exp_key)
+                .into_iter()
+                .map(F::from_canonical_u8)
+                .collect();
+            assert_eq!(key, exp_key);
+
+            let leaf_key: Vec<Vec<u8>> = rlp::decode_list(&node);
+            let nib = Nibbles::from_compact(&leaf_key[0]);
+            let exp_ptr = F::from_canonical_usize(MAX_KEY_NIBBLE_LEN - 1 - nib.nibbles().len());
+            assert_eq!(exp_ptr, ptr);
+        }
+        let key_id = compute_leaf_mapping_key_id(mapping_slot);
+        let value_id = compute_leaf_mapping_value_id(mapping_slot);
+        // Check values digest
+        {
+            let exp_digest =
+                compute_leaf_mapping_values_digest(&key_id, &value_id, &mapping_key, &value);
+            assert_eq!(pi.values_digest(), exp_digest.to_weierstrass());
+        }
+        // Check metadata digest
+        {
+            let exp_digest = compute_leaf_mapping_metadata_digest(&key_id, &value_id, mapping_slot);
+            assert_eq!(pi.metadata_digest(), exp_digest.to_weierstrass());
+        }
+        assert_eq!(pi.n(), F::ONE);
+    }
+}
