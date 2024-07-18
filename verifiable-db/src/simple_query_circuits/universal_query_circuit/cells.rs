92822f5a6f38323646ab65c1935bba4aaaf8afe3
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-07-18 13:40:48 +0300

    fix: ryhope API

--- verifiable-db/src/simple_query_circuits/universal_query_circuit/cells.rs
+++ verifiable-db/src/simple_query_circuits/universal_query_circuit/cells.rs
@@ -13,7 +13,7 @@ use plonky2::{
     iop::target::{BoolTarget, Target},
 };
 use ryhope::{
-    storage::memory::InMemory,
+    storage::{memory::InMemory, EpochKvStorage, TreeTransactionalStorage},
     tree::{sbbst, TreeTopology},
     InitSettings, MerkleTreeKvDb, NodePayload,
 };
@@ -59,7 +59,17 @@ pub(crate) fn build_cells_tree(
     // we create a dummy storage representing a sbbst tree with `input_len` elements;
     // the storage is fake becuase we don't store anything in the nodes, as we are just
     // interested in the tree topology
-    let fake_storage = MerkleTree::new(InitSettings::Reset(sbbst::Tree::empty()), ()).unwrap();
+    let mut fake_storage = MerkleTree::new(InitSettings::Reset(sbbst::Tree::empty()), ()).unwrap();
+
+    // Insert the `input_len` nodes in the tree
+    fake_storage
+        .in_transaction(|s| {
+            for i in 1..=input_len {
+                s.store(i, Payload(()))?;
+            }
+            Ok(())
+        })
+        .expect("failed to initialize fake storage");
 
     let root_key = fake_storage.root().unwrap();
     build_cells_subtree_at_key(
