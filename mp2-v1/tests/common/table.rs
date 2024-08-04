801a3400a865717d348ccbf0beaf49cf3ce2f57d
nikkolasg <nikkolasg@gmail.com>
2024-08-04 23:22:01 +0200

    debug

--- mp2-v1/tests/common/table.rs
+++ mp2-v1/tests/common/table.rs
@@ -213,16 +213,16 @@ impl Table {
         let cell_update = cell_tree
             .in_transaction(|t| {
                 for new_cell in update.updated_cells.iter() {
+                    let merkle_cell =
+                        cell::MerkleCell::new(new_cell.id, new_cell.value, update.primary);
                     println!(
                         " --- TREE: inserting rest-cell: (index {}) : {:?}",
                         self.columns.cells_tree_index_of(new_cell.id) + 1,
-                        new_cell
+                        merkle_cell
                     );
                     // here we don't put i+2 (primary + secondary) since only those values are in the cells tree
                     // but we put + 1 because sbbst starts at +1
                     let cell_key = self.columns.cells_tree_index_of(new_cell.id);
-                    let merkle_cell =
-                        cell::MerkleCell::new(new_cell.id, new_cell.value, update.primary);
                     match update_type {
                         TreeUpdateType::Update => t.update(cell_key, merkle_cell)?,
                         // This should only happen at init time or at creation of a new row
