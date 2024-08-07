09426e39c2fbdcbbe78e1d6f6b0a6426e2c066a7
nikkolasg <nikkolasg@gmail.com>
2024-08-07 18:07:22 +0200

    files

---
+++ mp2-v1/tests/common/cases/query.rs
@@ -0,0 +1,119 @@
+use std::ops::Add;
+
+use crate::common::{cases::indexing::BLOCK_COLUMN_NAME, rowtree::MerkleRowTree};
+
+use super::{
+    super::{context::TestContext, proof_storage::ProofStorage, table::Table},
+    MappingValuesExtractionArgs, TableSourceSlot, TestCase,
+};
+use anyhow::Result;
+use futures::{stream, StreamExt};
+use hashbrown::HashMap;
+use log::info;
+use mp2_v1::indexing::{block::BlockPrimaryIndex, row::Row};
+use ryhope::{storage::RoEpochKvStorage, Epoch};
+
+impl TestCase {
+    pub async fn test_query<P: ProofStorage>(&self, ctx: &TestContext<P>) -> Result<()> {
+        match self.source {
+            TableSourceSlot::Mapping((ref map, _)) => query_mapping(&ctx, map, &self.table).await?,
+            _ => unimplemented!("yet"),
+        }
+        Ok(())
+    }
+}
+async fn query_mapping<P: ProofStorage>(
+    ctx: &TestContext<P>,
+    map: &MappingValuesExtractionArgs,
+    table: &Table,
+) -> Result<()> {
+    let query = cook_query(ctx, map, table).await?;
+    info!("QUERY on the testcase: {query}");
+    let parsed = parsil::prepare(&query)?;
+    let pis = parsil::resolve::resolve(&parsed, table)?;
+    Ok(())
+}
+
+// cook up a SQL query on the secondary index. For that we just iterate on mapping keys and
+// take the one that exist for most blocks
+async fn cook_query<P: ProofStorage>(
+    ctx: &TestContext<P>,
+    map: &MappingValuesExtractionArgs,
+    table: &Table,
+) -> Result<String> {
+    let mut all_table = HashMap::new();
+    let max = table.row.current_epoch();
+    for epoch in max..0 {
+        let rows = collect_all_at(&table.row, epoch).await?;
+        for row in rows {
+            let epochs = all_table.entry(row.k.clone()).or_insert(Vec::new());
+            epochs.push(epoch);
+        }
+    }
+    // find the longest running row
+    let (longest_key, epochs) = all_table
+        .iter()
+        .max_by_key(|(row_key, epochs)| {
+            // simplification here to start at first epoch where this row was. Otherwise need to do
+            // longest consecutive sequence etc...
+            find_longest_consecutive_sequence(epochs.to_vec())
+        })
+        .expect("unable to find longest row?");
+    // now we can fetch the key that we want
+    let key_value = hex::encode(&longest_key.value.to_be_bytes_vec());
+    let key_column = table.columns.secondary.name.clone();
+    // Assuming this is mapping with only two columns !
+    let value_column = table.columns.rest[0].name.clone();
+    let table_name = table.name.clone();
+    // we set the block bounds
+    let longest_sequence = find_longest_consecutive_sequence(epochs.to_vec());
+    // TODO: careful about off by one error. -1 because tree epoch starts at 1
+    let min_block = *epochs.first().unwrap() as u64 + table.genesis_block - 1;
+    let max_block = min_block + longest_sequence as u64;
+    Ok(format!("SELECT AVG({value_column}) FROM {table_name} WHERE {BLOCK_COLUMN_NAME} > {min_block} AND {BLOCK_COLUMN_NAME} < {max_block} AND {key_column} == \\x{key_value};"))
+}
+
+async fn collect_all_at(tree: &MerkleRowTree, at: Epoch) -> Result<Vec<Row<BlockPrimaryIndex>>> {
+    let root_key = tree.root_at(at).await.unwrap();
+    let (ctx, payload) = tree.try_fetch_with_context_at(&root_key, at).await.unwrap();
+    let root_row = Row {
+        k: root_key,
+        payload,
+    };
+    let mut all_rows = vec![root_row];
+    let mut to_inspect = vec![ctx];
+    while !to_inspect.is_empty() {
+        let local = to_inspect.clone();
+        let (local_rows, local_ctx): (Vec<_>, Vec<_>) = stream::iter(local.iter())
+            .then(|ctx| async {
+                let lctx = ctx.clone();
+                let mut local_rows = Vec::new();
+                let mut local_ctx = Vec::new();
+                for child_k in lctx.iter_children().flat_map(|child_k| child_k) {
+                    let (child_ctx, child_payload) =
+                        tree.try_fetch_with_context_at(child_k, at).await.unwrap();
+                    local_rows.push(Row {
+                        k: child_k.clone(),
+                        payload: child_payload,
+                    });
+                    local_ctx.push(child_ctx.clone())
+                }
+                (local_rows, local_ctx)
+            })
+            .unzip()
+            .await;
+        all_rows.extend(local_rows.into_iter().flatten().collect::<Vec<_>>());
+        to_inspect = local_ctx.into_iter().flatten().collect::<Vec<_>>();
+    }
+    Ok(all_rows)
+}
+
+fn find_longest_consecutive_sequence(v: Vec<i64>) -> usize {
+    let mut longest = 0;
+    for i in 0..v.len() - 1 {
+        if v[i] == v[i + 1] + 1 {
+            longest += 1;
+        }
+    }
+    longest
+}
