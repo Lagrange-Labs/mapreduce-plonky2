0a44bf9772bcdde1478d65b1219318f574f38607
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-08-08 15:18:03 +0300

    WiP

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,7 @@ in
 
   # https://devenv.sh/packages/
   packages = [ pkgs.git pkgs.figlet pkgs.openssl pkgs.pkg-config ]
+             ++ lib.optionals config.devenv.isTesting [ pkgs.docker ]
              ++ lib.optionals pkgs.stdenv.targetPlatform.isDarwin [
                pkgs.libiconv
                pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
@@ -21,15 +22,17 @@ in
   # Make Go dependencies RW
   env.GOFLAGS = "-modcacherw";
   env.OPENSSL_DEV = pkgs.openssl.dev;
+  env.DB_URL = "host=localhost dbname=storage port=${builtins.toString config.env.PGPORT}";
 
   # https://devenv.sh/tests/
   enterTest = ''
     cargo test --features ci -- --test-threads 16
   '';
 
-  # https://devenv.sh/services/
+  # Spawn a local PgSQL instance iff we are not in test mode (e.g. when running
+  # `devenv up`) for development purposes.
   services.postgres = {
-    enable = true;
+    enable = !config.devenv.isTesting;
     listen_addresses = "127.0.0.1";
     port = lib.strings.toInt (orDefault config.env.PGSQL_PORT "5432");
     settings = {
@@ -43,10 +46,33 @@ in
 
   enterShell = ''
   figlet -f slant "MR2 loaded"
-  figlet -f standard -w200 "PgSQL on port ${builtins.toString config.env.PGSQL_PORT}"
   '';
 
   scripts.db.exec = "psql storage -h localhost -p ${builtins.toString config.env.PGPORT}";
+
+  # Run PgSQL in a container iff we are running `devenv test`. The goal is to be
+  # able to run multiple PgSQL at once on the same machine for concurrent CI
+  # runs on beefy servers.
+  processes.postgres-ci = lib.mkIf config.devenv.isTesting {
+    exec = (lib.concatStringsSep " " [
+      "${pkgs.docker}/bin/docker"
+      "--name postgres-${config.env.PGPORT}"
+      "run"
+      "-p ${config.env.PGPORT}:5432"
+      "postgres"
+    ]
+    );
+
+    process-compose = {
+      description = "docker container of postgres";
+      shutdown = {
+        command = "${pkgs.docker}/bin/docker rm -f postgres-${config.env.PGPORT}";
+      };
+      environment = [
+        "POSTGRES_DB=storage"
+      ];
+    };
+  };
 
   # https://devenv.sh/languages/
   languages.rust = {
@@ -63,9 +89,4 @@ in
     # commitizen.enable = true;
     # rustfmt.enable = true;
   };
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
