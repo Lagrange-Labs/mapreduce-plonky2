131304e7774a41d8ffdb725570ea8ae189914468
Insun35 <inseonyu7@gmail.com>
2024-08-16 17:33:46 +0900

    feat: add PI_LEN for binding public inputs

--- verifiable-db/src/results_tree/binding/mod.rs
+++ verifiable-db/src/results_tree/binding/mod.rs
@@ -1,2 +1,9 @@
+use mp2_common::F;
+use public_inputs::PublicInputs;
+
 pub(crate) mod binding_results;
 pub(crate) mod public_inputs;
+
+// Without this skipping config, the generic parameter was deleted when `cargo fmt`.
+#[rustfmt::skip]
+pub(crate) const PI_LEN: usize = PublicInputs::<F>::total_len();
