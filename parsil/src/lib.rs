0891a17eacc288a0533afa0a4ba0d927beb42cbf
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-08-23 20:24:29 +0300

    feat: add a helper function

--- parsil/src/lib.rs
+++ parsil/src/lib.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
+use sqlparser::ast::Query;
 use symbols::ContextProvider;
 pub use utils::parse_and_validate;
 pub use utils::ParsilSettings;
 pub use utils::PlaceholderSettings;
 pub use utils::DEFAULT_MAX_BLOCK_PLACEHOLDER;
 pub use utils::DEFAULT_MIN_BLOCK_PLACEHOLDER;
+use verifiable_db::query::aggregation::QueryBounds;
 
 pub mod assembler;
 pub mod bracketer;
@@ -25,4 +27,17 @@ mod visitor;
 /// criterion imposed by the current proving architecture.
 pub fn check<C: ContextProvider>(query: &str, settings: &ParsilSettings<C>) -> Result<()> {
     parse_and_validate(query, settings).map(|_| ())
+}
+
+/// Generate a SQL queries to fetch the keys and blocks where the conditions on
+/// the primary (and potentially secondary) index is satisfied.
+pub fn keys_in_index_boundaries<C: ContextProvider>(
+    query: &str,
+    settings: &ParsilSettings<C>,
+    bounds: &QueryBounds,
+) -> Result<Query> {
+    let mut q = parse_and_validate(query, settings)?;
+    q = isolator::isolate(&q, settings, bounds)?;
+    q = executor::generate_query_keys(&mut q, settings)?;
+    Ok(q)
 }
