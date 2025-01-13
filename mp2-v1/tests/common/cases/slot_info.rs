3be6ef1aaad09cbe5d88cdfab59c8cf495b4219d
Zack Youell <zackyouell@mac.com>
2025-01-13 13:47:46 +0000

    Fixed slot input lengths for test

--- mp2-v1/tests/common/cases/slot_info.rs
+++ mp2-v1/tests/common/cases/slot_info.rs
@@ -572,10 +572,10 @@ impl LargeStruct {
 
     pub fn slot_inputs(slot: u8) -> Vec<SlotInput> {
         vec![
-            SlotInput::new(slot, 0, 256, 0),
+            SlotInput::new(slot, 0, 32, 0),
             // Big-endian layout
-            SlotInput::new(slot, 16, 128, 1),
-            SlotInput::new(slot, 0, 128, 1),
+            SlotInput::new(slot, 16, 16, 1),
+            SlotInput::new(slot, 0, 16, 1),
         ]
     }
 }
