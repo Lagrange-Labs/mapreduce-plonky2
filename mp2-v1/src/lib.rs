75b3b3f5eb1d3748df4429f7d917595f0f97d889
nicholas-mainardi <mainardinicholas@gmail.com>
2025-02-17 13:33:17 +0100

    chore: add README.md and high-level description of main crates

--- mp2-v1/src/lib.rs
+++ mp2-v1/src/lib.rs
@@ -1,4 +1,30 @@
-//! Circuits for v1 of Lagrange Proof Network (LPN)
+//! Provide the generic APIs to build proving parameters and generate proofs for 
+//! the Zk-SQL coprocessor by Lagrange.
+//! 
+//! In a nutshell, Lagrange Zk-SQL coprocessor allows to run verifiable SQL queries 
+//! over tables in Lagrange verifiable DB. The verifiable DB allows to create 
+//! tables from blockchain data, altogether with a proof that the DB was constructed 
+//! with the same data extracted from the blockchain.
+//! 
+//! The high-level flow to provably build a table in the verifiable DB is:
+//! - An *extraction proof* is generated to compute a cryptographic accumulator of the data to 
+//! be employed to create a table; in case of blockchain data, this requires to prove the accumulator 
+//! was built from data found in on-chain data structures (e.g., storage MPT)
+//! - A *table creation* proof is generated to actually build the tables in Lagrange verifiable DB; 
+//! this proof is also checking that the data employed to build the table was the same data extracted 
+//! by the extraction proof, hinging upon the cryptographic accumulator
+//! 
+//! The verifiable DB supporst also updates to the tables, and the *table creation* proof can be 
+//! incrementally updated to prove the construction of the updated table. 
+//! Note that the verifiable DB currently supports append-only updates: indeed, to allow querying 
+//! over historical data, the DB is conceived as a time-series DB, and so each modification to the DB 
+//! implies that a new row is added to the table with the current timestamp (e.g., the block number 
+//! in case of blockchain data)
+//! 
+//! Once a table is created, queries can be provably run over the table. Running a query generates 
+//! a proof of correct computation of the query results. This proof is then recursively composed with 
+//! the *table creation proof*, generating the final proof of query execution. This final proof can 
+//! then be wrapped in a Groth16 proof for cheap on-chain verification.
 #![allow(incomplete_features)]
 #![allow(clippy::large_enum_variant)]
 // Add this to allow generic const expressions, e.g. `PAD_LEN(NODE_LEN)`.
