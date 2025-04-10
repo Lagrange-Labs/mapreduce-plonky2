462873360fa43229c849dc03437e3ec20fccb6ed
Franklin Delehelle <franklin.delehelle@odena.eu>
2025-04-10 18:19:59 +0300

    typo

--- devenv.nix
+++ devenv.nix
@@ -8,7 +8,7 @@ in
   cachix.enable = false;
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.figlet pkgs.openssl pkgs.pkg-config pkgs.cargo-limit pks.awscli2 ]
+  packages = [ pkgs.git pkgs.figlet pkgs.openssl pkgs.pkg-config pkgs.cargo-limit pkgs.awscli2 ]
              ++ lib.optionals config.devenv.isTesting [ pkgs.docker ]
              ++ lib.optionals pkgs.stdenv.targetPlatform.isDarwin [
                pkgs.libiconv
